b526cadbdffecb87bdd25ef1222a2a0d0acb48b6
Sergey <gavrin_s@mail.ru>
2018-04-27 12:50:39 +0700

    Initial commit

---
+++ program.py
@@ -0,0 +1,16 @@
+from vk_api8 import VKApi
+
+login = "+79081182712"
+password = "petia9379992"
+client = "6251514" #VK App client id
+scope = "friends,messages,groups" #OPTIONAL
+version = "5.69" #Api version OPTIONAL
+
+api = VKApi(login, password, client, scope, version)
+
+id = 155128139
+user_friends = api.get_friends_ids(id)
+friends_count = user_friends['count']
+print(friends_count)
+friends_ids = user_friends['items']
+print(friends_ids)
